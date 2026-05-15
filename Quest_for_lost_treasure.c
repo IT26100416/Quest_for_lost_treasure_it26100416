5ad3267756695999d63d368c346e1e70a65d84fd
IT26100416 <it26100416@my.sliit.lk>
2026-05-15 10:49:06 +0530

    Added doors and hidden traps

--- Quest_for_lost_treasure.c
+++ Quest_for_lost_treasure.c
@@ -8,8 +8,10 @@
 #define TREASURE 'T'
 #define HEALTH 'H'
 #define KEY 'K'
+#define DOOR 'D'
 
 char map[MAP_SIZE][MAP_SIZE];
+int hiddenTrap[MAP_SIZE][MAP_SIZE];
 
 void fn_initializeMap()
 {
@@ -19,6 +21,7 @@ void fn_initializeMap()
 		for(int c = 0; c < MAP_SIZE; c++)
 		{
 			map[r][c] = EMPTY;
+			hiddenTrap[r][c] = 0;
 		}
 	}
 
@@ -37,7 +40,6 @@ void fn_initializeMap()
 	{
 		int r = rand() % MAP_SIZE;
 		int c = rand() % MAP_SIZE;
-
 		if(map[r][c] == EMPTY && r > 0 && r < MAP_SIZE - 1 && c > 0 && c < MAP_SIZE - 1)
 		{
 			map[r][c] = WALL;
@@ -52,7 +54,7 @@ void fn_initializeMap()
 	
 		int r = rand() % MAP_SIZE;
 		int c = rand() % MAP_SIZE;
-		if(map[r][c] == EMPTY && r > 0 && r < MAP_SIZE - 1 && c > 0 && c < MAP_SIZE - 1)
+		if(map[r][c] == EMPTY)
 		{
 
 			map[r][c] = TREASURE;
@@ -62,11 +64,11 @@ void fn_initializeMap()
 
 	//Random health packs
 	int placedHealth = 0;
-	while (placedHealth < 3)
+	while (placedHealth < 5)
 	{
 		int r = rand() % MAP_SIZE;
 		int c = rand() % MAP_SIZE;
-		if(map[r][c] == EMPTY && r > 0 && r < MAP_SIZE - 1 && c > 0 && c < MAP_SIZE - 1)
+		if(map[r][c] == EMPTY)
 		{
 			map[r][c] = HEALTH;
 			placedHealth++;
@@ -81,10 +83,38 @@ void fn_initializeMap()
 	{
 		int r = rand() % MAP_SIZE;
 		int c = rand() % MAP_SIZE;
-		if(map[r][c] == EMPTY && r > 0 && r < MAP_SIZE - 1 && c > 0 && c < MAP_SIZE - 1)
+		if(map[r][c] == EMPTY)
 		{
 			map[r][c] = KEY;
 			placedKeys++;
+		}
+
+	}
+	
+	// Random doors
+	int placedDoors = 0;
+	while (placedDoors < 3)
+	{
+		int r = rand() % MAP_SIZE;
+		int c = rand() % MAP_SIZE;
+		if(map[r][c] == EMPTY)
+		{
+			map[r][c] = DOOR;
+			placedDoors++;
+		}
+
+	}
+
+	//Hidden traps
+	int placedTraps = 0;
+	while (placedTraps < 10)
+	{
+		int r = rand() % MAP_SIZE;
+		int c = rand() % MAP_SIZE;
+		if(map[r][c] == EMPTY && hiddenTrap[r][c] == 0)
+		{
+			hiddenTrap[r][c] = 1;
+			placedTraps++;
 		}
 
 	}
